696f8b80068a0c5f6ee81927c6c1b0a3005c6707
mrkreddy5 <rkreddy55665@gmail.com>
2025-08-15 19:05:09 +0530

    merge overlapping intervals

--- Arrays/2025-08-15/Merge_OverLapping_Intervals.java
+++ Arrays/2025-08-15/Merge_OverLapping_Intervals.java
@@ -2,10 +2,45 @@ import java.util.*;
 
 public class Merge_OverLapping_Intervals {
     public static void main(String[] args) {
-        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}}
-    }
+        int[][] intervals = {{1,12},{2,6},{8,10},{15,18}};
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0], b[0]));
 
-    public static void merge(int[][] intervals){
-        Arrays.sort(intervals, )
+        List<int[]> mergedList = new ArrayList<>();
+
+        int[] current = intervals[0];
+        
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0] <= current[1]){
+                current[1] = Math.max(current[1], intervals[1][1]);
+            }
+            else{
+                mergedList.add(current);
+                current = intervals[i];
+            }
+        }
+        mergedList.add(current);
+        
+        for(int[] interval : mergedList){
+            for(int n : interval){
+                System.out.print(n+" ");
+            }
+            System.out.println();
+        }
+
+        /* T.C = O(n*logn) and S.C = O(n) for storing in a list */
+        /* In place merging without using extra storage S.C = O(1) */
+
+        // int idx =0;
+        // for(int i=1;i<intervals.length;i++){
+        //     if(intervals[i][0] <= intervals[idx][1]){
+        //         intervals[idx][1] = Math.max(intervals[idx][1], intervals[1][1]);
+        //     }
+        //     else{
+        //         idx++;
+        //         intervals[idx] = intervals[i];  // if not overlapping copy that interval to the next of merged ones
+        //     }
+        // }
+
+        // print from 0 to idx only 
     }
 }
