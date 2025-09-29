2b517c1962a010e283a7b17acdc003cbf3b8395a
mrkreddy5 <rkreddy55665@gmail.com>
2025-09-30 00:19:38 +0530

    Book Allocation

---
+++ Binary Search/2025-09-27/Aggressive_Cows.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Aggressive_Cows {
+    public static void main(String[] args) {
+        int[] nums = {0, 3, 4, 7, 10, 9};
+        int k = 4;
+        System.out.println(min_dist(nums, k));
+    }
+
+    public static int min_dist(int[] nums, int k) {
+        Arrays.sort(nums);
+        int low = 1; 
+        int high = nums[nums.length - 1] - nums[0];
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int count = 1; 
+            int lastPlaced = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] - lastPlaced >= mid) {
+                    count++;
+                    lastPlaced = nums[i];
+                }
+            }
+
+            System.out.println(low + ":" + mid + ":" + high + ":" + count);
+
+            if (count >= k) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
+
+/*
+   given array is unsorted , so we have to sort it fast
+   according to prob statement there are k aggressive cows
+   and the given arr contains locations where cows can drink water
+   the aggressive cows should minimum among maximum possibility
+   Input: n = 6, k = 4, nums = [0, 3, 4, 7, 10, 9]
+
+    Output: 3
+
+    Explanation: The maximum possible minimum distance between any two cows will be 3 when 4 cows are placed at positions [0, 3, 7, 10]. 
+    Here the distances between cows are 3, 4, and 3 respectively. We cannot make the minimum distance greater than 3 in any ways
+ */
---
+++ Binary Search/2025-09-28/Book_Allocation.java
@@ -0,0 +1,77 @@
+
+public class Book_Allocation {
+    public static void main(String[] args) {
+        int[] nums = {12, 34, 67, 90};
+        int m = 2;
+        System.out.println(minimized_maximum(nums,m));
+    }
+
+    public static int minimized_maximum(int[] nums, int m){
+        if(m>nums.length) return -1;
+        else{
+            int[] min_max = min_max(nums);
+            if(m==nums.length) return min_max[0];
+
+            int low = min_max[0];
+            int high = min_max[1];
+            int ans = high;
+            System.out.println(low + ":" + high);
+            while(low<=high){
+                int mid = low + (high-low)/2;
+                
+                if(canAllocate(nums, mid, m)){
+                    ans = mid;
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            // System.out.println(ans);
+            return ans;
+        }
+    }
+
+    public static boolean canAllocate(int[] nums, int mid , int m){
+        int currP =0;
+        int studC =1;
+        for(int p : nums){
+            if(p>mid){
+                return false;
+            }
+            if(currP + p <= mid){
+                currP += p;
+            }
+            else{
+                currP = p;
+                studC++;
+                if(studC >m) return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static int[] min_max(int[] nums){
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int n : nums){
+            if(n>max) max = n;
+            sum +=n;
+        }
+        return new int[]{max,sum};
+    }
+}
+
+
+/*
+    According to problem student every student atleast get 1 book , we have to allocate books where the student gets highest pages is minimum
+    from example : 25, 46, 28, 49, 24 , m =4
+    definitely a student should get 2 books 
+    25 + 46 = 71
+    46 + 28 = 74
+    28 + 49 = 77
+    49 + 24 = 73
+    among all this 71 is the minimum among max 
+    so answer will always lie between max(arr) and sum(arr)
+ */
