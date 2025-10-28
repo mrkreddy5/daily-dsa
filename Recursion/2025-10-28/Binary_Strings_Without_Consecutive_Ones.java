aff9ee7bc94bd00a19ecd900b6e2f492770f14f5
mrkreddy5 <rkreddy55665@gmail.com>
2025-10-29 00:02:44 +0530

    valid parentheses generation

--- Recursion/2025-10-28/Binary_Strings_Without_Consecutive_Ones.java
+++ Recursion/2025-10-28/Binary_Strings_Without_Consecutive_Ones.java
@@ -22,4 +22,25 @@ public class Binary_Strings_Without_Consecutive_Ones {
             binary_strings(n, curr + "1", res);
         }
     }
+
+    // Below code is more efficient as strings are immutable wasting more space in above code
+
+    public static void binary_strings_sb(int n, StringBuilder curr, List<String> res) {
+        if (curr.length() == n) {
+            res.add(curr.toString());
+            return;
+        }
+
+        // Always can add '0'
+        curr.append('0');
+        binary_strings_sb(n, curr, res);
+        curr.deleteCharAt(curr.length() - 1);
+
+        // Add '1' only if last char is not '1'
+        if (curr.length() == 0 || curr.charAt(curr.length() - 1) != '1') {
+            curr.append('1');
+            binary_strings_sb(n, curr, res);
+            curr.deleteCharAt(curr.length() - 1);
+        }
+    }
 }
--- Recursion/2025-10-28/N_sized_valid_parentheses.java
+++ Recursion/2025-10-28/N_sized_valid_parentheses.java
@@ -35,7 +35,7 @@ public class N_sized_valid_parentheses {
 
     // The above code generates every combination even it is not a valid one and validation is overhead for them
     // whereas in below code only valid ones are generated
-    
+
     public static void parentheses_generate_optimised(int n,int open, int close,  String curr, List<String> res){
         if(curr.length() == 2*n){
             res.add(curr);
@@ -44,6 +44,27 @@ public class N_sized_valid_parentheses {
 
         if(open<n) parentheses_generate_optimised(n, open+1, close, curr + '(', res);
         if(close<open) parentheses_generate_optimised(n, open, close+1, curr + ')', res);
+    }
+
+    // In above methods we are using strings which are immutable , everytime a new string is created memory wastage
+    // where as in below method we are using a string builder which just add and delete the char , which makes it more faster compared to above method
+
+    public static void parentheses_generate_sb(int n,int open, int close,  StringBuilder curr, List<String> res){
+        if(curr.length() == 2*n){
+            res.add(curr.toString());
+            return;
+        }
+
+        if(open<n) {
+            curr.append('(');
+            parentheses_generate_sb(n, open+1, close, curr, res);
+            curr.deleteCharAt(curr.length()-1);
+        }
+        if(close<open) {
+            curr.append(')');
+            parentheses_generate_sb(n, open, close+1, curr, res);
+            curr.deleteCharAt(curr.length()-1);
+        }
     }
 
 }
