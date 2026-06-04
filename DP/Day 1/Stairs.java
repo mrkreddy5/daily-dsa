b4a52818b6698aab6c783de36373cbb05ecbb5ce
ramkrishna524 <126253401+mrkreddy5@users.noreply.github.com>
2026-06-05 00:23:44 +0530

    Refactor climbStairs for optimized space complexity
    Refactored climbStairs method to improve space complexity from O(n) to O(1) by using two variables instead of an array.

---
+++ DP/Day 1/Stairs.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
+
+
+
+// it is like fibonacci series, so no need of entire array
+// we just need last 2 
+
+public int climbStairs(int n) {
+    if(n <= 1) return 1;
+
+    int prev2 = 1;
+    int prev1 = 1;
+
+    for(int i = 2; i <= n; i++){
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+}
+
+// it improves S.c from O(n) to O(1)
